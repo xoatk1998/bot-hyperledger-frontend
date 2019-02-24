1ef4dd2084a2ae5baed8c03dbacceed8d6cccb84
xoatk1998 <43579602+xoatk1998@users.noreply.github.com>
2019-02-24 17:57:22 +0700

    Import NavT

---
+++ src/components/NavT.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+class NavT extends Component{
+    render(){
+        return(
+            <div className="container-fluid col-2 sidenav">
+                <div className="info">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-home fa-2x"></i>
+                                <span>Home</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-user-circle fa-2x align-self-center"></i>
+                                <span>Account</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-bank fa-2x align-self-center"></i>
+                                <span>Wallet</span>
+                            </a>
+                        </li>
+                    </ul>
+                    
+                </div>
+                <div className="transaction">
+                    <h5>Transaction</h5>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-money fa-2x"></i>
+                                <span>Payin</span>
+                            </a>
+                        </li>
+                        <li> 
+                            <a href="#">
+                                <i className="fa fa-exchange fa-2x"></i>
+                                <span>PayToll</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="introduce">
+                <h5>About</h5>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-users fa-2x"></i>
+                                <span>HyperTeam</span>
+                            </a>
+                        </li>
+                        <li> 
+                            <a href="#">
+                                <i className="fa fa-cogs fa-2x"></i>
+                                <span>PayToll</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}  
+export default NavT
