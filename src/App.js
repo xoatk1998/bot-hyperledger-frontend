cb69ee21e39c86940f001679264cde6cf9ad5452
tmt98 <you@example.com>
2019-02-25 14:43:52 +0700

    account

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from './components/SignIn';
 import Header from './components/Header'
 import NavT from './components/NavT'
 import Home from "../src/components/home"
+import Account from './components/Account'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Footer from './components/footer';
@@ -12,13 +13,24 @@ class App extends Component {
   render() {
     return (
       <div>
-      {/* <SignIn/> */}
-        
         <Header />
-        
+        <div className="container-fluid row" style={{"display":"flex"}}>
         <NavT />
-        {/* <Home /> */}
-        
+        <Account />
+          {/* <div className="">
+            <NavT />
+          </div>
+          <div className="col-8">
+            <Account />
+          </div> */}
+        {/* <SignIn/> */}
+          
+          {/* <Header />
+          
+          <NavT /> */}
+          {/* <Home /> */}
+          
+        </div>
       </div>
     );
   }
---
+++ src/components/Account.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react'
+
+class Account extends Component{
+    render(){
+        return(
+            <div className = "container account">
+                <div className="container-fluid row OwnerInfo">
+                    <span className="col-3">Thông tin chủ tài khoản</span>
+                    <div className="col-9 row info">
+                        <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                            
+                            <div className="form-group">
+                                <label>Họ Tên</label>
+                                <input id="fullname" className="form-control" type="text" value="Nguyễn Đức Thiện" readOnly/>
+                            </div>
+                            <div className="form-group">
+                                <label>Địa chỉ</label>
+                                <input id="fullname" className="form-control" type="text" value="Mỹ Đình, Nam Từ Liêm,Hà Nội" readOnly/>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <div className="form-group">
+                                <label>Số CMND/Căn cước công dân</label>
+                                <input id="fullname" className="form-control" type="text" value="033098001763" readOnly/>
+                            </div>
+                            <div className="form-group">
+                                <label>SĐT</label>
+                                <input id="fullname" className="form-control" type="text" value="0978776056" readOnly/>
+                            </div>
+                        </div>
+                        
+                    </div>
+                
+                </div>
+
+                <div className="container row VehicleInfo">
+                <span className="col-3">Thông tin phương tiện</span>
+                    <div className="col-9 row info">
+                        <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                            
+                            <div className="form-group">
+                                <label>Biển số xe </label>
+                                <input id="fullname" className="form-control" type="text" value="89E1-42493" readOnly/>
+                            </div>
+                            <div className="form-group">
+                                <label>Model</label>
+                                <input id="fullname" className="form-control" type="text" value="Blade" readOnly/>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <div className="form-group">
+                                <label>Ngày sản xuất</label>
+                                <input id="fullname" className="form-control" type="text" value="02/06/2006" readOnly/>
+                            </div>
+                            <div className="form-group">
+                                <label>Ngày đăng kí</label>
+                                <input id="fullname" className="form-control" type="text" value="05/08/2006" readOnly/>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Account
