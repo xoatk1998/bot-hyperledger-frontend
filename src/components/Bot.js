a94a85252be446f398d63f0a4a2a1819f0940dd6
Shin <shin@ubuntu.ubuntu-domain>
2019-02-27 11:47:50 +0700

    add-sidebar

---
+++ src/components/Bot.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class Bot extends Component{
+    render(){
+        return(
+            <div className="container-fluid  row header">
+               <h4>This is BOT</h4>
+            </div>
+        )
+    }
+}
+export default Bot
---
+++ src/components/PayIn.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class PayIn extends Component{
+    render(){
+        return(
+            <div className="container-fluid  row header">
+               <h4>This is PayIn</h4>
+            </div>
+        )
+    }
+}
+export default PayIn
