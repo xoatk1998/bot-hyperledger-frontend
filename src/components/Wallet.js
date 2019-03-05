144034273d105c05d474738049b7dca770ad513a
tmt98 <you@example.com>
2019-03-05 15:02:42 +0700

    updateWallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import History from './History'
 import RegisterVehicle from './RegisterVehicle'
+import {Redirect} from 'react-router-dom'
 import "../css/wallet.css"
 
 // import { css } from '@emotion/core';
@@ -24,6 +25,9 @@ class Wallet extends Component {
             licensePlate:null,
             calledApi:false,
             HasWallet:false,
+            numberWallet:0,
+            walletArray:null,
+            selectValue:0
 
             
 
@@ -36,10 +40,11 @@ class Wallet extends Component {
           }).then((res) => {
             if(res.data.length > 0)  {
                 this.setState({
-                    accountBalance : res.data[0].accountBalance,
-                    licensePlate : res.data[0].licensePlate,
+                    accountBalance : res.data[this.state.numberWallet].accountBalance,
+                    licensePlate : res.data[this.state.numberWallet].licensePlate,
                     info : res.data,
                     calledApi:true,
+                    walletArray:res.data
                 })
                 console.log(this.state.info)                
             }
@@ -68,7 +73,24 @@ class Wallet extends Component {
             )
         }
     }
+    renderOption(){
+        if(this.state.walletArray===null) return <div />
+        else{
+            let option = this.state.walletArray.map((value,key)=>{
+                return (<option value={key} key={key}>  Xe {key + 1}  </option>)
+            })
+            return <select className="custom-select" style={{ "width": "85px" }} defaultValue={this.state.selectValue} onChange={(val) => { this.setState({ numberWallet: val.target.value }, () => { this.componentDidMount() }) }} >
+
+                {
+                    option
+                }
+
+
+            </select>
+        }
+    }
     render() {
+        if(this.state.register) return <Redirect to="/register-vehicle"></Redirect>
         const { error } = this.state;
         if (error) {
             return <div>error</div>
@@ -92,15 +114,23 @@ class Wallet extends Component {
                 )
             else{
                 // if (this.state.info !== null)
-                if(this.state.info.length == 0 || this.state.accountBalance===undefined) return <RegisterVehicle />
+                if(this.state.info.length == 0 || this.state.accountBalance===undefined) return <Redirect to="/register-vehicle" />
                 else{
                     return (
                         <div className="container" style ={{"backgroundColor": "#eaeaea78"}}>
                             <div className=" row justify-content-center">
                                 <div className="col-sm-10 ">
                                     <div className="box_thongtin_sodu_ud">
-                                        <div className="text_tieude_all_ud">Credit Information </div>
-                                        <hr style={{"width" :"20%"}}/>
+                                        <div className="row">
+                                            <div className="col-10">
+                                                <div className="text_tieude_all_ud">Credit Information </div>
+                                                <hr style={{"width" :"20%"}}/>
+                                            </div>
+                                            <div className="col-2">
+                                                <button className="btn btn-info " onClick={()=>{this.setState({register:true})}}>Add Wallet</button>
+                                            </div>
+                                        </div>
+                                        {this.renderOption()}
                                         <div className="box_all_tien_user_ud">
                                             Account Balance :
                                 
